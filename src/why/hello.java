55fcebe3e6672bb6af1f885d5c2d253d17707d7f
robertBuckless <robert.buckless@maine.edu>
2018-02-01 14:06:45 -0500

    HERE

---
+++ src/why/hello.java
@@ -0,0 +1,9 @@
+package why;
+
+public class hello {
+	public static void main(String[] args){
+		System.out.println("Hear ye: Important Anncouncement");
+		System.out.println("Hello World");
+		System.out.println("Good Bye");
+	}
+}
