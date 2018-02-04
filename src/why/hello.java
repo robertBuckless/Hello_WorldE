d49c25d5323059d9e598c2ef76c767f269bdb34b
robertBuckless <robert.buckless@maine.edu>
2018-02-04 12:40:55 -0500

    Denise updates

--- src/why/hello.java
+++ src/why/hello.java
@@ -6,6 +6,7 @@ public class hello {
 		System.out.println("Hello World");
 		System.out.println("Hi Alice");
 		System.out.println("Hi Bob");
+		System.out.println("Hi Dernise");
 		System.out.println("Good Bye");
 	}
 }
