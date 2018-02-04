087f07f7bd930dd24d3b14b44be70798d779c184
robertBuckless <robert.buckless@maine.edu>
2018-02-04 12:38:21 -0500

    Charlie's changes

--- src/why/hello.java
+++ src/why/hello.java
@@ -6,6 +6,7 @@ public class hello {
 		System.out.println("Hello World");
 		System.out.println("Hi Alice");
 		System.out.println("Hi Bob");
+		System.out.println("Hi Charlie");
 		System.out.println("Good Bye");
 	}
 }
