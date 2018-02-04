39cef088f8234d1624b00ac89495bcb9d76cde65
robertBuckless <robert.buckless@maine.edu>
2018-02-04 12:16:31 -0500

    Up date

--- src/why/hello.java
+++ src/why/hello.java
@@ -4,6 +4,8 @@ public class hello {
 	public static void main(String[] args){
 		System.out.println("Hear ye: Important Anncouncement");
 		System.out.println("Hello World");
+		System.out.println("Hi Alice");
+		System.out.println("Hi Bob");
 		System.out.println("Good Bye");
 	}
 }
